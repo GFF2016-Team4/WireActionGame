9adb06bcdb048a74eea9e36c709fb25437e4079c
HOME-PC\tomohiro <tom0430@docomo.ne.jp>
2016-10-19 16:17:51 +0900

    テスト

---
+++ Assets/sample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("テスト");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
