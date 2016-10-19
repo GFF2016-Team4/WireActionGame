a480f9f7e0a2093700552692cb1adb02e33f2891
chulos <latale_kaeru@yahoo.co.jp>
2016-10-19 16:39:54 +0900

    TZZZZZZZZzzzz

---
+++ Assets/SSSSAMPLEEEE.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SSSSAMPLEEEE : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("TTTTTTTTTTTTTESAEEEEEEEEEEEEEEEE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
