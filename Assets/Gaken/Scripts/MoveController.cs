d7d6e2ac31b0ee99dd885ec25dc8a6dd1e7dc182
RedTheCreator <623695056@qq.com>
2016-10-26 21:53:11 +0900

    Add files via upload

---
+++ Assets/Gaken/Scripts/MoveController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour
+{
+    public float moveSpeed = 6.0f;
+    public float jumpForce = 300f;
+    public float gravity = 10.0f;
+
+    private float velocityY = 0;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 playerRotation;
+    private Vector3 cameraRotation;
+    public Camera camera;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        //Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 velocity = Vector3.zero;
+        transform.Rotate(camera.transform.rotation.x, 0, 0);
+
+        velocityY -= gravity * Time.deltaTime;
+
+        //velocity = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
+
+        playerRotation = transform.rotation.eulerAngles;
+        cameraRotation = camera.transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(playerRotation.x, cameraRotation.y, playerRotation.z);
+
+        if (controller.isGrounded)
+        {
+            velocityY = 0;
+
+            //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //moveDirection = transform.TransformDirection(moveDirection);
+            //moveDirection *= moveSpeed;
+            velocity = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                velocityY = jumpForce;
+            //transform.GetComponent<Rigidbody>().AddForce(transform.up * jumpForce);
+        }
+
+        velocity.y = velocityY;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Gaken/Scripts/bulletController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletController : MonoBehaviour {
+
+	public float force = 300f;
+
+	void Start () {
+		transform.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+	}
+	
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+		transform.GetComponent<Rigidbody> ().isKinematic = true;      
+	}
+}
