b0dae0546903bd0ca2a2b1030a04900d1e8c7abd
chulos <latale_kaeru@yahoo.co.jp>
2016-11-02 17:17:47 +0900

    きのこ

---
+++ Assets/Oishi/Script/kinoko.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kinoko : MonoBehaviour
+{
+    public GameObject targetPos;
+    public GameObject kinokoPos;
+    public float speed;
+    public float Power;     //ジャンプ力
+    public float limit;     //ジャンプする時間
+    private bool isJump;        //ジャンプしてるか
+    private float Jumptemp;     //Jumplimit保存用
+
+    // Use this for initialization
+    void Start()
+    {
+        isJump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Rigidbody rd = GetComponent<Rigidbody>();
+        rd.AddForce(x * speed, 0, z * speed);
+
+        //ジャンプ処理
+        if (isJump == true && limit >= 0)
+        {
+            //rd.AddForce(Vector3.up * Power);
+
+            limit -= 1.0f;
+            Debug.Log("ジャンプ中です");
+        }
+        if (limit <= 0)
+        {
+            isJump = false;
+            limit = Jumptemp;
+        }
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += transform.forward * 0.1f;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position -= transform.forward * 0.1f;
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Rotate(0, 10, 0);
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Rotate(0, -10, 0);
+        //}
+    }
+
+    //Jumpタグオブジェクトに接触したら
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" && isJump == false)
+        {
+            isJump = true;
+            Jumptemp = limit;
+
+            Debug.Log("ジャンプします");
+        }
+    }
+}
--- Assets/nagumo/Player/PlayerMove.cs
+++ Assets/nagumo/Player/PlayerMove.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : MonoBehaviour
+{
 
     public float speed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
