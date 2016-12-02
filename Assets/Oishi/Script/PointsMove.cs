e0f68368e174b472149f6b58401abda18e3a865f
unknown <>
2016-12-02 22:43:16 +0900

    指定した複数地点を通る移動(試運転)

---
+++ Assets/Oishi/Script/PointsMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PointsMove : MonoBehaviour {
+
+	public GameObject oyaObj;
+	private GameObject[] childObj;
+	private Vector3[] childPos;
+
+	public int moveTime;
+
+	int childcount;
+
+	// Use this for initialization
+	void Start ()
+	{
+		childcount = oyaObj.transform.childCount;
+
+		childPos=new Vector3[childcount];
+		childObj = new GameObject[childcount];
+
+
+		for(int i =0; i< childcount; i++)
+		{
+			childObj[i]=oyaObj.transform.GetChild(i).gameObject;
+			childPos [i] = childObj [i].transform.position;
+		}
+	}
+	void Update ()
+	{	
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Move ();
+		}
+	}
+
+	void Move()
+	{
+		iTween.MoveTo (gameObject, iTween.Hash ("path", childPos,
+												"time", moveTime));
+											 
+	}
+}
