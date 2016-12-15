f272334df430792dc80f67b73319bf87b0f4745f
unknown <>
2016-12-15 20:05:37 +0900

    腕、複製

---
+++ Assets/nagumo/Instantiate4/colorManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorManager : MonoBehaviour {
+
+	[Header("徐々にアルファ値を変更する時の速さ")]
+	public float slowlyAlpha;
+
+	Color m_Color;
+	Color m_resetColor;
+
+	void Start()
+	{
+		m_Color = new Color (0, 0, 0, slowlyAlpha);
+		m_resetColor = new Color (0, 0, 0, 1);
+	}
+
+	//徐々に減らす
+	public void minusAlpha(Renderer renderer)
+	{
+		renderer.material.color -= m_Color;
+	}
+	//徐々に増やす
+	public void plusAlpha(Renderer renderer)
+	{
+		renderer.material.color += m_Color;
+	}
+    //アルファ値を0に
+    public void minusResetAlpha(Renderer renderer)
+	{
+		renderer.material.color -= m_resetColor;
+	}
+	//アルファ値を1に
+	public void plusResetAlpha(Renderer renderer)
+	{
+		renderer.material.color += m_resetColor;
+	}
+}
---
+++ Assets/nagumo/Instantiate4/fukusei.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fukusei : MonoBehaviour {
+
+	//複製元のオブジェクト
+	public GameObject orgObj;
+
+    //public Transform parent;
+
+    //複製オブジェクト
+    private GameObject fksObj;
+
+	//複製元オブジェクトのRenderer
+	Renderer org_renderer;
+	//複製オブジェクトのRenderer
+	Renderer fks_renderer;
+
+	colorManager m_colorManager;
+
+	bool isDestroy;
+
+    public float d_timer;
+    float destroyTimer;
+
+    //テスト用
+    //Vector3 testposition;
+
+
+	void Start ()
+	{
+		m_colorManager = GetComponent<colorManager> ();
+		org_renderer = orgObj.GetComponent<Renderer> ();
+		destroyTimer = 0;
+
+		//テスト用
+		//testposition = new Vector3 (5f, 0f, 0f);
+	}
+	
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{ 
+			fksObj = Instantiate (orgObj, orgObj.transform.position /*+ testposition*/, 
+                orgObj.transform.rotation)as GameObject;
+            fksObj.transform.localScale = new Vector3(20, 20, 20);
+            //Instantiate(orgObj, parent, false);
+
+            //複製オブジェクトのRendererを取得
+            fks_renderer = fksObj.GetComponent<Renderer> ();
+            //アルファ初期値を0に設定
+            m_colorManager.minusResetAlpha(fks_renderer);
+            //m_colorManager.plusResetAlpha(fks_renderer);
+
+            orgObj.GetComponent<BoxCollider> ().enabled = false;
+		}
+
+		if (fksObj != null) 
+		{
+            orgObj.GetComponent<SkinnedMeshRenderer>().enabled = false;
+
+            destroyTimer += Time.deltaTime;
+
+			if (destroyTimer >= d_timer) 
+			{
+				isDestroy = true;
+                orgObj.GetComponent<SkinnedMeshRenderer>().enabled = true;
+            }
+
+			if (isDestroy == false && org_renderer.material.color.a > 0) 
+			{
+                //複製オブジェクトのアルファを徐々に増やす
+                m_colorManager.plusAlpha(fks_renderer);
+                //複製元オブジェクトのアルファを徐々に減らす
+                m_colorManager.minusAlpha (org_renderer);
+			}
+			if (isDestroy == true)
+			{
+                orgObj.GetComponent<BoxCollider>().enabled = true;
+                fksObj.GetComponent<BoxCollider> ().enabled = false;
+
+				//複製オブジェクトのアルファを徐々に減らす
+				m_colorManager.minusAlpha (fks_renderer);
+				//複製元オブジェクトのアルファを徐々に増やす
+				m_colorManager.plusAlpha (org_renderer);
+
+				//複製オブジェクトのアルファ値が0になったら削除
+				if (fks_renderer.material.color.a <= 0)
+				{
+					Destroy (fksObj);
+
+					destroyTimer = 0;
+
+					isDestroy = false;
+				}
+			}
+		}
+	}
+}
