6da57aacb7216f04a273e69a4c461722de30ee69
unknown <>
2016-12-16 12:31:24 +0900

    Enemy腕スクリプト修正

--- Assets/nagumo/Instantiate4/fukusei.cs
+++ Assets/nagumo/Instantiate4/fukusei.cs
@@ -39,8 +39,8 @@ public class fukusei : MonoBehaviour {
 	
 	void Update ()
 	{
-		if (Input.GetKeyDown (KeyCode.Space)) 
-		{ 
+        if (Input.GetKeyDown (KeyCode.Space)) 
+		{
 			fksObj = Instantiate (orgObj, orgObj.transform.position /*+ testposition*/, 
                 orgObj.transform.rotation)as GameObject;
             fksObj.transform.localScale = new Vector3(20, 20, 20);
@@ -49,39 +49,41 @@ public class fukusei : MonoBehaviour {
             //複製オブジェクトのRendererを取得
             fks_renderer = fksObj.GetComponent<Renderer> ();
             //アルファ初期値を0に設定
-            m_colorManager.minusResetAlpha(fks_renderer);
-            //m_colorManager.plusResetAlpha(fks_renderer);
+            //m_colorManager.minusResetAlpha(fks_renderer);
+            m_colorManager.plusResetAlpha(fks_renderer);
 
             orgObj.GetComponent<BoxCollider> ().enabled = false;
 		}
 
 		if (fksObj != null) 
 		{
-            orgObj.GetComponent<SkinnedMeshRenderer>().enabled = false;
-
+            //orgObj.GetComponent<SkinnedMeshRenderer>().enabled = false;
+            
             destroyTimer += Time.deltaTime;
 
 			if (destroyTimer >= d_timer) 
 			{
 				isDestroy = true;
-                orgObj.GetComponent<SkinnedMeshRenderer>().enabled = true;
+                //orgObj.GetComponent<SkinnedMeshRenderer>().enabled = true;
             }
 
 			if (isDestroy == false && org_renderer.material.color.a > 0) 
 			{
                 //複製オブジェクトのアルファを徐々に増やす
-                m_colorManager.plusAlpha(fks_renderer);
+                //m_colorManager.plusAlpha(fks_renderer);
                 //複製元オブジェクトのアルファを徐々に減らす
-                m_colorManager.minusAlpha (org_renderer);
-			}
+                //m_colorManager.minusAlpha(org_renderer);
+                m_colorManager.minusResetAlpha(org_renderer);
+            }
 			if (isDestroy == true)
 			{
                 orgObj.GetComponent<BoxCollider>().enabled = true;
                 fksObj.GetComponent<BoxCollider> ().enabled = false;
 
-				//複製オブジェクトのアルファを徐々に減らす
-				m_colorManager.minusAlpha (fks_renderer);
+                //複製オブジェクトのアルファを徐々に減らす
+                m_colorManager.minusAlpha (fks_renderer);
 				//複製元オブジェクトのアルファを徐々に増やす
+                if(org_renderer.material.color.a <= 1)
 				m_colorManager.plusAlpha (org_renderer);
 
 				//複製オブジェクトのアルファ値が0になったら削除
