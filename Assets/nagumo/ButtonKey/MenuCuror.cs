ba38561e52906f783370f9afa14a58e1edb53bfc
unknown <>
2017-01-17 13:46:22 +0900

    カーソル移動音

--- Assets/nagumo/ButtonKey/MenuCuror.cs
+++ Assets/nagumo/ButtonKey/MenuCuror.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 public class MenuCuror : MonoBehaviour {
 
     private RectTransform m_RectTransform;
+    private Vector2 m_Curor;
 
     // Use this for initialization
     void Start () {
@@ -12,22 +13,33 @@ public class MenuCuror : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //カーソル移動音
+        //if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) ||
+        //    Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        //    SoundManager.Instance.PlaySE(AUDIO.SE_enter2);
 
 	}
 
+    //カーソル移動
     void Awake()
     {
         m_RectTransform = GetComponent<RectTransform>();
     }
 
+    //カーソル移動
     void LateUpdate()
     {
         GameObject selectedObject = EventSystem.current.currentSelectedGameObject;
+        m_Curor = m_RectTransform.anchoredPosition;
 
         if (selectedObject == null)
         {
             return;
         }
+
+        if (m_Curor != selectedObject.GetComponent<RectTransform>().anchoredPosition)
+            PlaySE_move();
+
         m_RectTransform.anchoredPosition =
             selectedObject.GetComponent<RectTransform>().anchoredPosition;
     }
@@ -38,5 +50,10 @@ public class MenuCuror : MonoBehaviour {
         //再生
         //audiosource.Play();
         SoundManager.Instance.PlaySE(AUDIO.SE_enter1);
+    }
+
+    void PlaySE_move()
+    {
+        SoundManager.Instance.PlaySE(AUDIO.SE_enter2);
     }
 }
