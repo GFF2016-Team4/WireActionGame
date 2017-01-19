8d13adc8dc562f6cdc8aaecf7861c89544e87d60
ralsoon <smallkirby423@yahoo.co.jp>
2017-01-19 19:00:53 +0900

    当たり判定の変更

--- Assets/Abe/Player/Scripts/Player.cs
+++ Assets/Abe/Player/Scripts/Player.cs
@@ -178,6 +178,7 @@ public class Player : MonoBehaviour, RopeEventHandlar
         if(!IsGround())                  return;
         if(!Input.GetButtonDown("Jump")) return;
 
+        //SoundManager.Instance.PlaySE
         playerVelocity += Vector3.up * jumpPower;
         playerVelocity += cameraInfo.GetInputVelocity()*2.0f;
         controller.Move(playerVelocity * Time.deltaTime);
