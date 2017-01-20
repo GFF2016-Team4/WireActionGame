854c555e11f07f07d99528d3c0f92bd3d50eb3d8
unknown <>
2017-01-20 14:49:11 +0900

    パーティクル

--- Assets/nagumo/AttackPattern/Script/WalkSE.cs
+++ Assets/nagumo/AttackPattern/Script/WalkSE.cs
@@ -2,20 +2,26 @@
 using System.Collections;
 
 public class WalkSE : MonoBehaviour {
+    public GameObject m_Somke;
 
-	// Use this for initialization
-	void Start () {
+    private GameObject exp;
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+       
+    }
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag == "Field")
-        SoundManager.Instance.PlaySE(AUDIO.SE_Enemy_Walk);
+        if (col.tag == "Field")
+        {
+            SoundManager.Instance.PlaySE(AUDIO.SE_Enemy_Walk);
+            exp = (GameObject)Instantiate(m_Somke.gameObject, transform.position,
+                Quaternion.identity);
+        }
     }
 }
