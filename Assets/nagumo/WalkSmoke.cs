711599d2b488a8efa6fc8fcbc88124ae0e594b1d
unknown <>
2017-01-20 14:57:04 +0900

    上げ忘れ

---
+++ Assets/nagumo/WalkSmoke.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkSmoke : MonoBehaviour {
+    private ParticleSystem ps;
+	// Use this for initialization
+	public void Start () {
+        ps = GetComponent<ParticleSystem>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ps.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
