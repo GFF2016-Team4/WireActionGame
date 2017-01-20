05b7038d1beadcdecfe0f6552187fbe25e4b461c
chulos <latale_kaeru@yahoo.co.jp>
2017-01-20 15:42:21 +0900

    ロードシーン

---
+++ Assets/Oishi/Script/LoadAnm.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadAnm : MonoBehaviour
+{
+
+    public Animator m_animator;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_animator.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine("Load");
+            //m_animator.Play(m_animator.ToString());
+
+            Debug.Log(StartCoroutine("Load"));
+        }
+
+    }
+
+    IEnumerator Load()
+    {
+        m_animator.Play("Oishi/Scene/Load/LoadAnimation");
+
+        yield return null;
+    }
+}
