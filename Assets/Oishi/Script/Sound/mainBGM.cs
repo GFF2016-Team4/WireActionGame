ebb29aeca6eb4c90b8854cc9a78788be59cb052b
chulos <latale_kaeru@yahoo.co.jp>
2017-02-03 11:36:25 +0900

    titleBGM,mainBGM追加

---
+++ Assets/Oishi/Script/Sound/mainBGM.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainBGM : MonoBehaviour 
+{
+    void Start()
+    {
+        SoundManager.Instance.PlayBGM(AUDIO.BGM_Stage);
+    }
+}
---
+++ Assets/Oishi/Script/Sound/titleBGM.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class titleBGM : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.Instance.PlayBGM(AUDIO.BGM_Title);
+    }
+}
