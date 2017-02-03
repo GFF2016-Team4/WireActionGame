e37cd340c3ed3dd71c725e981f8de0b3e6a1b64c
ralsoon <smallkirby423@yahoo.co.jp>
2017-02-03 12:01:07 +0900

    DebugLogがかなり多いのでShowToggleDebugを追加 showDebugのチェックをはずすと出てこないようになる

---
+++ Assets/Abe/Other/Script/ShowToggleDebug.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShowToggleDebug : MonoBehaviour
+{
+    [SerializeField, Tooltip("説明文")]
+    bool showDebug;
+
+
+    void Awake()
+    {
+        Debug.logger.logEnabled = showDebug;
+    }
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
