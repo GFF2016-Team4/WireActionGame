888b08f90064adf32b4782912d6234b26227da28
chulos <latale_kaeru@yahoo.co.jp>
2017-02-03 10:46:05 +0900

    画面を揺らす

---
+++ Assets/Oishi/Script/Shake/shakeObj.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shakeObj : MonoBehaviour
+{
+
+    public GameObject shakeObject;
+    public GameObject Pcamera;
+    public float x;
+    public float y;
+    public float time;
+
+    Vector3 tempPos;
+    void Start()
+    {
+        shakeObject.transform.localPosition = shakeObject.transform.parent.GetComponent<PlayerCamera>().offset;
+    }
+
+    void Update()
+    {
+        shakeObject.transform.parent.GetComponent<PlayerCamera>().offset = shakeObject.transform.localPosition;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        tempPos = shakeObject.transform.parent.GetComponent<PlayerCamera>().offset;
+        iTween.PunchPosition(shakeObject, iTween.Hash("x", x, "y", y, "time", time));
+    }
+}
