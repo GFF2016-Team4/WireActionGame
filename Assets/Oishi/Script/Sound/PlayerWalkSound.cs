af42ecab7a724f2c4bf4d48308786f6792d29825
chulos <latale_kaeru@yahoo.co.jp>
2017-02-03 11:09:40 +0900

    足音追記

---
+++ Assets/Oishi/Script/Sound/PlayerWalkSound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWalkSound : MonoBehaviour
+{
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Field")
+        {
+            SoundManager.Instance.PlaySE(AUDIO.SE_playerLanding);
+        }
+    }
+}
