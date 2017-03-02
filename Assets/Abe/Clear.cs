e3511eb67b20530f162c32672230e324ea21f3d8
ralsoon <smallkirby423@yahoo.co.jp>
2017-03-02 20:07:44 +0900

    BGMの不具合を修正

---
+++ Assets/Abe/Clear.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Clear : MonoBehaviour
+{
+    //[SerializeField, Tooltip("説明文")]    
+    void Start()
+    {
+        SoundManager.Instance.PlayBGM(AUDIO.SE_Clear);
+    }
+}
---
+++ Assets/Abe/GameOverBGM.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameOverBGM : MonoBehaviour
+{
+    //[SerializeField, Tooltip("説明文")]
+    void Start()
+    {
+        SoundManager.Instance.PlayBGM(AUDIO.SE_GameOver);
+    }
+}
